82e9d47c861b948b8eabf7d337501a6076ce930c
Milon Mahato <milonmahato67@gmail.com>
2021-05-10 18:20:03 +0600

    fixed urls for cros errors.

--- albums/urls.py
+++ albums/urls.py
@@ -9,6 +9,6 @@ router.register("albums", AlbumViewSet, basename="albums")
 app_name = "albums"
 urlpatterns = [
     path("", include(router.urls)),
-    path("my-albums/<str:id>", GetOwnAlbum.as_view(), name="my-albums"),
-    path("upload-photo/<str:id>", UploadPhoto.as_view(), name="upload-photo"),
+    path("my-albums/<str:id>/", GetOwnAlbum.as_view(), name="my-albums"),
+    path("upload-photo/<str:id>/", UploadPhoto.as_view(), name="upload-photo"),
 ]
